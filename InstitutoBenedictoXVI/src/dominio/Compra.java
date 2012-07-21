a290485039327406a547a6fad682f36da9abbd18
Gian Carlo Morin <gian_carlo_morin@hotmail.com>
2012-07-21 18:52:05 -0500

    Envio 1 GMORIN

--- InstitutoBenedictoXVI/src/dominio/Compra.java
+++ InstitutoBenedictoXVI/src/dominio/Compra.java
@@ -1,5 +1,32 @@
+
+
+
 package dominio;
 
-public class Compra {
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
+
+public class Compra {
+public enum Estado {EN_PROCESO, ENVIADA, CONFIRMADA, ENTREGADA, PAGADA}
+	
+	private int codigo;
+	private Proveedor habitacion;
+	private List<Producto> material = new ArrayList<Producto>();
+	private Estado estado;
+	private Date fechaGeneracion;
+	private Date fechaEntrega;	
+	private Double monto;
+	
+	
 }
+
+
+
+
+
+
+
+
+    
