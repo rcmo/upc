23ec2884e126651d5ba5fa5425882603a5f90de9
rcmo <carlos_0483@hotmail.com>
2012-07-22 11:59:06 -0500

    Agregar

---
+++ InstitutoBenedictoXVI/test/Dominiotest/UsuarioTest.java
@@ -0,0 +1,15 @@
+package Dominiotest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UsuarioTest {
+
+	@Test
+	public void test() {
+	//fail("Not yet implemented");
+		
+	}
+
+}
