9cddad015acb87901d4a2976928884b321079f19
Fabio <U201121647@UPC.EDU.PE>
2012-07-26 21:06:30 -0500

    actualizar1

--- InstitutoBenedictoXVI/src/dominio/Usuario.java
+++ InstitutoBenedictoXVI/src/dominio/Usuario.java
@@ -1,5 +1,22 @@
 package dominio;
 
-public class Usuario {
+public class Usuario extends Persona{
+	
+	private String fechapase;
+	
+	public Usuario(){
+		super("","","","","","","","","");
+		}
+
+	public Usuario(String dni, String nombres, String apellido_paterno,
+			String apellido_materno, String telefono, String correo,
+			String direccion, String distrito, String departamento) {
+		super(dni, nombres, apellido_paterno, apellido_materno, telefono, correo,
+				direccion, distrito, departamento);
+		// TODO Auto-generated constructor stub
+		
+	}
+	
+	
 
 }
--- InstitutoBenedictoXVI/test/test/UsuarioTest.java
+++ InstitutoBenedictoXVI/test/test/UsuarioTest.java
@@ -3,14 +3,40 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import dominio.Clientealta;
+import dominio.Usuarioalta;
 
 public class UsuarioTest {
 
 	@Test
-	public void test() {
-	//fail("Not yet implemented");
+	
+	public void AltaUsuarios(){
+		
+		
+		String nombre = "carlos";
+		String ape_paterno=null;
+		String ape_materno= null;
+		String email = null;
+		String dni = null;
+		String telefono = null;
+		String cargo = null;
+		
+		Usuarioalta alta = new Usuarioalta();
+		
+
+		boolean datoscorrectos = alta.ValidaDatos(nombre,ape_paterno,ape_materno,email,dni,telefono,cargo); 
+		
+		if (!datoscorrectos){
+			System.out.print("No ingreso datos a los campos");
+			
+		}
+		//Assert.assertTrue("No a ingresado datos relevantes",datoscorrectos);
 		
 	}
+	//fail("Not yet implemented");
+		
 
 }
